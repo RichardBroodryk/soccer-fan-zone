e5a7c8adad459f6ceb253b20fdd2a9c9d19806d3
Richard Broodryk <rugbyanthemzone@outlook.com>
2026-06-09 10:43:52 +0200

    Update Android branding and launcher icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.soccerfanzone.app',
-  appName: 'soccer-fan-zone',
+  appName: 'Soccer Fan Zone',
   webDir: 'build'
 };
 
